7fd24b7beff3bbfcc7c0d799f4dae72b5350d91c
ALVARLEUNG <alvar.leung@qq.com>
2019-07-19 16:58:11 +0800

    [LST]feat:finish the story Multi Counter

---
+++ src/components/CounterGroup.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <p> {{number}}</p>
+        <button type="danger" @click="add()">点我+</button>
+        <button type="danger" @click="minus()">点我-</button>
+        <br/><br/>
+    </div>
+</template>
+<script>
+    export default {
+        name: "CounterGroup",
+        props: ['calculate'],
+        data() {
+            return {
+                number: 0
+            };
+        },
+        methods: {
+            add() {
+                this.number++;
+               this.$emit('calculate', 1)
+            },
+            minus() {
+                this.number--;
+                this.$emit('calculate', -1)
+            }
+        }
+    }
+
+</script>
+
+<style scoped>
+
+</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,47 +1,66 @@
 <template>
-  <div class="hello">
-    <p>  {{number}}</p>
-    <button type="danger" @click="add()">点我+</button>
-    <button type="danger" @click="minus()">点我-</button>
-  </div>
+    <div class="hello">
+        <el-input type="number" placeholder="请输入数字" v-model="inputNumber"></el-input>
+        <div v-for="n in parseInt(counterNumber)" :key="n">
+            <CounterGroup v-on:calculate="calculate"></CounterGroup>
+        </div>
+        <el-input v-model="totalCount"></el-input>
+    </div>
 </template>
 
 <script>
-export default {
-  data () {
-    return {
-      number: 0
-    };
-  },
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  },
-  methods: {
-     add (){
-      this.number++;
-    },
-     minus() {
-      this.number--;
-    }
-  }
-}
+    import CounterGroup from './CounterGroup.vue'
+
+    export default {
+        data() {
+            return {
+                inputNumber: '',
+                totalCount: 0,
+                counterNumber: 0
+                // length: parseInt(this.inputNumber)
+            };
+        },
+        components: {
+            CounterGroup
+        },
+        name: 'HelloWorld',
+        props: {
+            msg: String
+        },
+        methods: {
+          calculate (number) {
+              this.totalCount +=number;
+
+          }
+        },
+        mounted() {
+          this.counterNumber = this.inputNumber;
+        },
+        watch: {
+            inputNumber (newCount) {
+              return this.counterNumber = newCount;
+            }
+        }
+        }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+    h3 {
+        margin: 40px 0 0;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    a {
+        color: #42b983;
+    }
 </style>
